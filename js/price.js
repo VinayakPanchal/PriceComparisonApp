ee0fc6fa42fd73d8a115f9d22da10249d41ae8de
VinayakPanchal <vinayakpanchal007@outlook.com>
2017-01-30 03:10:58 -0500

    Final Commit

---
+++ js/price.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#showCategory').hide();
+    
+
+
+
+    $('#searchCategory').click(function(){
+        $('#showCategory').toggle(1500);
+
+    });
+});
+$(document).ready(function() {
+    $('#showFilter').hide();
+    
+
+
+
+    $('#searchFilter').click(function(){
+        $('#showFilter').toggle(1500);
+
+    });
+});
